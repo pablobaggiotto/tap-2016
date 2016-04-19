638e9e2d524fe24b1371413fe6908fe9905b0ecb
Pablo <Pablo>
2016-04-18 21:38:55 -0300

    meu trabalho g1

---
+++ Ordenação/Ordenacao/AlgoritmosOrdenacao.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menus
+{
+    public class AlgoritmosOrdenacao
+    {
+
+        public int[] InsertionSort(int[] vetor)
+        {
+            int i, j, atual;
+            for (i = 1; i < vetor.Length; i++) // percorre vetor da esquerda para a direita
+            {
+                atual = vetor[i];
+                j = i;
+                while ((j > 0) && (vetor[j - 1] > atual)) // alinha os valores da sua esquerda
+                {
+                    vetor[j] = vetor[j - 1];
+                    j = j - 1;
+                }
+                vetor[j] = atual;
+            }
+            return vetor; // retorna vetor
+        }
+
+
+        public int[] selectionSort(int[] vetor)
+        {
+            int min, aux;
+
+            for (int i = 0; i < vetor.Length - 1; i++) // percorre o vetor vasculhando repetidamente os numeros e escolhe o menor valor
+            {
+                min = i;
+
+                for (int j = i + 1; j < vetor.Length; j++) // comparando o valor minimo com o proximo
+                    if (vetor[j] < vetor[min]) // compara se o proximo numero é menor que o primeiro numero
+                        min = j; // se for menor ele passa a ser o primeiro
+
+                if (min != i) // menor valor será passado para o início do vetor, e depois o segundo menor valor para a segunda posição e assim sucessivamente
+                {
+                    aux = vetor[min];
+                    vetor[min] = vetor[i];
+                    vetor[i] = aux;
+                }
+            }
+
+            return vetor; // retorna o vetor
+        }
+
+
+
+        public int[] quickSort(int[] vetor)
+        {
+            int inicio = 0;
+            int fim = vetor.Length - 1;
+
+            quickSort(vetor, inicio, fim); // chamando o quick sort
+
+            return vetor;
+        }
+
+        private static void quickSort(int[] vetor, int inicio, int fim)
+        {
+            if (inicio < fim) // Comparando a primeira posição com a ultima, se a ultima for menor troca com a primeira posição
+            {
+                int p = vetor[inicio];
+                int i = inicio + 1; 
+                int f = fim;
+
+                while (i <= f) // Segunda posição com a ultima e faz a comparação
+                {
+                    if (vetor[i] <= p)
+                    {
+                        i++;
+                    }
+                    else if (p < vetor[f])
+                    {
+                        f--;
+                    }
+                    else
+                    {
+                        int troca = vetor[i]; // troca de posição se a ultima for menor que a primeira
+                        vetor[i] = vetor[f];
+                        vetor[f] = troca;
+                        i++;
+                        f--;
+                    }
+                }
+
+                vetor[inicio] = vetor[f]; // volta ao inicio com as posições ja organizadas
+                vetor[f] = p;
+
+                quickSort(vetor, inicio, f - 1);
+                quickSort(vetor, f + 1, fim);
+            }
+        }
+
+    }
+
+}
---
+++ Ordenação/Ordenacao/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace menus // criando o menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[100]; // declarando o vetor de 100 posicões
+
+            Random rnd = new Random(); // criando aleatoriamente as 100 posição por Random
+            for (int i = 0; i < 100; i++) // for do Random
+            {
+                vetor[i] = rnd.Next(0, 100);
+            }
+
+            AlgoritmosOrdenacao ordenar = new AlgoritmosOrdenacao(); // Importando da classe AlgoritmosOrdenação.cs
+            int opcao = 0;
+            do
+            {
+                Console.WriteLine("Escolha o algoritmo"); // Implementação do Menu
+                Console.WriteLine("1. Insertion Sort"); // Implementação do Menu
+                Console.WriteLine("2. Quick Sort"); // Implementação do Menu
+                Console.WriteLine("3. Selection Sort"); // Implementação do Menu
+                Console.WriteLine("Qualquer outra opção: Sair"); // Implementação do Menu
+                int.TryParse(Console.ReadLine(), out opcao); // Implementação do Menu
+
+                if (opcao == 0) // Opção de saida
+                {
+                    Console.WriteLine("Saindo..."); // Implementação do Menu
+                }
+                else // Opção de outra escolha
+                {
+                    if (opcao == 1) // Primeira Opção de escolha
+                    {
+                        // FUNCTION INSERTION SORT
+                        var resultado = ordenar.InsertionSort(vetor);
+                        for (int i = 0; i < resultado.Length; i++)
+                        {
+                            Console.Write(resultado[i] + ",");
+                        }
+                        Console.WriteLine("");
+                    }
+                    else if (opcao == 2) // Opção de outra escolha ou Segunda Opção
+                    {
+
+
+                        // FUNCTION QUICK SORT
+                        var resultado2 = ordenar.quickSort(vetor);
+                        for (int i = 0; i < resultado2.Length; i++)
+                        {
+                            Console.Write(resultado2[i] + ",");
+                        }
+                        Console.WriteLine("");
+                    }
+
+                    else if (opcao == 3) // Opção de outra escolha ou Terceira Opção
+                    {
+                        // FUNCTION SELECTION SORT 
+                        var resultado3 = ordenar.selectionSort(vetor);
+                        for (int i = 0; i < resultado3.Length; i++)
+                        {
+                            Console.Write(resultado3[i] + ",");
+                        }
+                        Console.WriteLine("");
+                    }
+                }
+
+            } 
+            while (opcao != 0);
+        }
+    }
+}
